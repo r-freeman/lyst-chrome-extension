77cf7c3f5368293d04cbcff9ca3946b4d4a1470c
Ryan Freeman <N00173767@student.iadt.ie>
2019-10-07 21:28:57 +0100

    Added comments and changed return type in parsexpath function

--- src/parsexpath.js
+++ src/parsexpath.js
@@ -1,3 +1,8 @@
+/**
+ * Parses the DOM looking for a set of nodes for a given expression
+ * @param expression
+ * @returns {null}
+ */
 export const parseXPath = (expression) => {
     let node, nodes = []
     let result = document.evaluate(
@@ -7,15 +12,13 @@ export const parseXPath = (expression) => {
         XPathResult.ANY_TYPE,
         null
     )
-
+    // iterate to kick things off
     node = result.iterateNext()
     while(node){
+        // push each node onto the nodes array
         nodes.push(node)
         node = result.iterateNext()
     }
-
-    // handy for finding attributes on a node, remove later
-    console.log(nodes)
-
-    return nodes[0]
+    // return the first matching node
+    return typeof nodes[0] !== 'undefined' ? nodes[0] : null
 }
