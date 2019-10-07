1693b35418c354f3cd112c97e3b10ae88f06e827
Ryan Freeman <N00173767@student.iadt.ie>
2019-10-07 22:47:50 +0100

    Added functionality to find the Amazon store region

--- src/content.js
+++ src/content.js
@@ -2,15 +2,20 @@ import {parseXPath} from "./parsexpath";
 
 try {
     // look for an asinId on the page
-    let asinId = parseXPath("(//input[@id='ASIN']/@value)[1]")
+    let asinId = parseXPath("//input[@id='ASIN']/@value")
     if (asinId !== null) {
         // asinId found, get the product details
         chrome.storage.local.set({
             'product': {
                 'asinId': asinId.value,
+                'storeRegion': location.href.match('.(\\uk|\de|\it|\fr|\es)')[1].toUpperCase(),
                 'url': location.href,
                 'title': parseXPath("//span[@id='productTitle']").innerText,
-                'price': parseXPath("(//span[@class='a-color-price'])[1]").innerHTML
+                'price': parseXPath(
+                    "//div[@class='a-section']/span[@id='price_inside_buybox']" +
+                    "|//span[@class='inlineBlock-display']/span" +
+                    "|//span[@id='priceblock_ourprice']"
+                ).innerText
             }
         }, () => {
             chrome.storage.local.get('product', (product) => {
