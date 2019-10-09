116fe2cb1385e3d85453892f0ac13598a30e4c0a
Ryan Freeman <N00173767@student.iadt.ie>
2019-10-09 23:13:32 +0100

    Refactored content.js and added MutationObserver to monitor the DOM for changes

--- src/content.js
+++ src/content.js
@@ -1,27 +1,63 @@
 import {parseXPath} from "./parsexpath";
 
-try {
-    // look for an asinId on the page
-    let asinId = parseXPath("//input[@id='ASIN']/@value")
-    if (asinId !== null) {
-        // asinId found, get the product details
-        chrome.storage.local.set({
-            'product': {
-                'asinId': asinId.value,
-                'storeRegion': location.href.match('.(\\uk|\de|\it|\fr|\es)')[1].toUpperCase(),
-                'url': location.href,
-                'title': parseXPath("//span[@id='productTitle']").innerText,
-                'price': parseXPath(
+let asinId = null
+
+/**
+ * Get product from the DOM
+ * @returns {null|{product: *, price: *, title: *, storeRegion: *, url: *}}
+ */
+const getProduct = () => {
+    try {
+        // look for an asinId on the page
+        asinId = parseXPath("(//input[@id='ASIN']/@value)[1]")
+        if (asinId !== null) {
+            // asinId found return the rest of the product details
+            return {
+                asinId: asinId.value,
+                storeRegion: location.href.match('.(\\uk|\de|\it|\fr|\es)')[1].toUpperCase(),
+                url: location.href,
+                title: parseXPath("//span[@id='productTitle']").innerText,
+                price: parseXPath(
                     "//div[@class='a-section']/span[@id='price_inside_buybox']" +
                     "|//span[@class='inlineBlock-display']/span" +
-                    "|//span[@id='priceblock_ourprice']"
+                    "|//span[@id='priceblock_ourprice']" +
+                    "|//span[@id='priceblock_dealprice']" +
+                    "|//span[@id='priceblock_saleprice']"
                 ).innerText
             }
-        }, () => {
-            chrome.storage.local.get('product', (product) => {
-                console.log(product)
-            })
-        })
+        } else {
+            return null
+        }
+    } catch (e) {
+        return null
     }
-} catch (e) {
 }
+
+/**
+ * Store product using the Chrome Storage API
+ * @param product
+ */
+const storeProduct = (product = getProduct()) => {
+    chrome.storage.local.set({product: product})
+}
+
+storeProduct()
+
+if(asinId !== null) {
+    // Use MutationObserver to watch for changes in the DOM
+    // For example, the DOM is changed when the user selects a different version of an item
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    const observerCallback = () => {
+        // store product again if change was found
+        storeProduct()
+    }
+
+    const observer = new MutationObserver(observerCallback);
+    observer.observe(
+        // Observe the child nodes in title_feature_div for changes
+        document.getElementById('title_feature_div'), {
+            childList: true,
+        }
+    )
+}
+
