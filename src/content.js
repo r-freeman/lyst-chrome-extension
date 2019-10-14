d922ca03745eaaaec3dc568010c66cd1efbf292e
Ryan Freeman <info@ryanfreeman.io>
2019-10-14 21:51:17 +0100

    Refactored content.js, added amazon.json containing xpath strings. Adjusted MutationObserver to capture product more accurately

--- src/content.js
+++ src/content.js
@@ -1,36 +1,36 @@
 import {parseXPath} from "./utils/xpath";
 import {numToDecimal} from './utils/numbers'
+import * as amazon from './amazon'
 
-let asinId = null
-
+let product = null
 /**
  * Get product from the DOM
  * @returns {null|{product: *, price: *, title: *, storeRegion: *, url: *}}
  */
 const getProduct = () => {
     try {
-        // look for an asinId on the page
-        asinId = parseXPath("(//input[@id='ASIN']/@value)[1]")
-        if (asinId !== null) {
-            // asinId found return the rest of the product details
-            return {
-                asinId: asinId.value,
-                storeRegion: location.href.match('.(\\uk|\de|\it|\fr|\es)')[1].toUpperCase(),
-                url: location.href,
-                title: parseXPath("//span[@id='productTitle']").innerText,
-                price: numToDecimal(parseXPath(
-                    "//div[@class='a-section']/span[@id='price_inside_buybox']" +
-                    "|//span[@class='inlineBlock-display']/span" +
-                    "|//span[@id='priceblock_dealprice']" +
-                    "|//span[@id='priceblock_saleprice']" +
-                    "|//span[@id='priceblock_ourprice']"
-                    // strip all non-alphanumeric characters from price
-                ).innerText.replace(/\D/g,''),2)
-            }
+        // look for an asinId and price on the page
+        product = {
+            asinId: parseXPath(amazon.productAsinId).value,
+            price: parseXPath(amazon.productPrice)
+        }
+
+        // check if asinId is not null and price is valid
+        if (product.asinId !== null && product.price.innerText.indexOf('-') === -1) {
+            // strip all non-alphanumeric characters from price and convert integer to decimal
+            product.price = numToDecimal(product.price.innerText.replace(/\D/g, ''), 2)
+            // add more details to the product
+            product.storeRegion = location.href.match('.(\\uk|\de|\it|\fr|\es)')[1].toUpperCase()
+            product.url = location.href
+            product.title =  parseXPath(amazon.productTitle).innerText
+            product.image = parseXPath(amazon.productImage).value
+
+            return product
         } else {
             return null
         }
     } catch (e) {
+        // Something went wrong return null
         return null
     }
 }
@@ -45,7 +45,7 @@ const storeProduct = (product = getProduct()) => {
 
 storeProduct()
 
-if(asinId !== null) {
+if (product !== null) {
     // Use MutationObserver to watch for changes in the DOM
     // For example, the DOM is changed when the user selects a different version of an item
     // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
@@ -57,8 +57,9 @@ if(asinId !== null) {
     const observer = new MutationObserver(observerCallback);
     observer.observe(
         // Observe the child nodes in title_feature_div for changes
-        document.getElementById('title_feature_div'), {
+        document.getElementById('centerCol'), {
             childList: true,
+            subtree: true
         }
     )
 }
