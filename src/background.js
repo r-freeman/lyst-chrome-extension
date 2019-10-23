a0e356bc55a7cccdb2012b9e8b32cd092cc80e88
Ryan Freeman <N00173767@student.iadt.ie>
2019-10-23 22:33:55 +0100

    Changed to Vue

--- src/background.js
+++ src/background.js
@@ -1,25 +1,59 @@
-import * as amazon from './amazon'
-
+import * as amazon from './amazon.json'
 /**
- * Background scripts listen for events in the background
- * https://developer.chrome.com/extensions/background_pages
+ * Background script to manage what happens when tabs change
  */
-const background = () => {
-    // execute the content script if the current url is Amazon
-    chrome.webNavigation.onCompleted.addListener(() => {
-        chrome.tabs.executeScript({
-            file: './static/js/content.js'
-        }, () => {
-            console.log(chrome.runtime.lastError)
-        })
-    }, {url: [{urlMatches:amazon.storeUrl}]})
+const backgroundTabs = () => {
+  // functions to change the toolbar icons
+  const setIcons = {
+    activeIcons: () => {
+      chrome.browserAction.setIcon({
+        path: {
+          "16": "icons/icon16.png",
+          "32": "icons/icon32.png"
+        }
+      })
+    },
+    defaultIcons: () => {
+      chrome.browserAction.setIcon({
+        path: {
+          "16": "icons/default_icon16.png",
+          "32": "icons/default_icon32.png"
+        }
+      })
+    }
+  }
 
-    chrome.runtime.onInstalled.addListener(() => {
-        // We'll check this value later and display an intro page
-        chrome.storage.local.set({hasSeenIntro: false})
-    });
+  // check if the current tab url is Amazon
+  const isTabAmazon = (url = '') => {
+    url.match(amazon.storeUrl) !== null ? setIcons.activeIcons() : setIcons.defaultIcons()
+  }
+
+  // check the current active tab url
+  const checkActiveTab = () => {
+    chrome.tabs.query({'active': true}, (activeTabs) => {
+      let activeTab = activeTabs[0]
+      // url is undefined when opening a new tab
+      activeTab.url === undefined ? isTabAmazon() : isTabAmazon(activeTab.url)
+    })
+  }
+
+  // when the user clicks on a current tab
+  chrome.tabs.onActivated.addListener((activeInfo) => {
+    chrome.tabs.get(activeInfo.tabId, (tab) => {
+      // check if the tab url is Amazon
+      isTabAmazon(tab.url)
+    })
+  })
+
+  // when a tab is updated
+  chrome.tabs.onUpdated.addListener(() => {
+    checkActiveTab()
+  })
+
+  // when a new tab is created
+  chrome.tabs.onCreated.addListener(() => {
+    checkActiveTab()
+  })
 }
 
-background()
-
-
+backgroundTabs()
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,119 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const ejs = require('ejs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtensionReloader = require('webpack-extension-reloader');
+const { VueLoaderPlugin } = require('vue-loader');
+const { version } = require('./package.json');
 
-const HtmlPlugin = ({template, inject, filename, chunks}) => {
-    return (
-        new HtmlWebpackPlugin(
-            {
-                template,
-                inject,
-                filename,
-                chunks,
-                minify: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    keepClosingSlash: true,
-                    minifyJS: true,
-                    minifyCSS: true,
-                    minifyURLs: true
-                }
-            }
-        )
-    )
+const config = {
+  mode: process.env.NODE_ENV,
+  context: __dirname + '/src',
+  entry: {
+    'background': './background.js',
+    'popup/popup': './popup/popup.js',
+  },
+  output: {
+    path: __dirname + '/build',
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loaders: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.sass$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader?indentedSyntax'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '/images/',
+          emitFile: false,
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '/fonts/',
+          emitFile: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      global: 'window',
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'icons', to: 'icons', ignore: ['icon.xcf'] },
+      { from: 'popup/popup.html', to: 'popup/popup.html', transform: transformHtml },
+      {
+        from: 'manifest.json',
+        to: 'manifest.json',
+        transform: (content) => {
+          const jsonContent = JSON.parse(content);
+          jsonContent.version = version;
+
+          if (config.mode === 'development') {
+            jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
+          }
+
+          return JSON.stringify(jsonContent, null, 2);
+        },
+      },
+    ]),
+  ],
+};
+
+if (config.mode === 'production') {
+  config.plugins = (config.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+  ]);
 }
 
-module.exports = {
-    entry: {
-        background: './src/background.js',
-        backgroundTabs: './src/backgroundTabs.js',
-        popup: './src/popup.js',
-        content: './src/content.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'static/js/[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(png|jpeg|jpg)$/,
-                loader: 'file-loader'
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        HtmlPlugin({
-            template: 'public/popup.html',
-            inject: 'body',
-            filename: 'popup.html',
-            chunks: ['popup']
-        }),
-        // HtmlPlugin({
-        //     template: 'public/intro.html',
-        //     filename: 'intro.html'
-        // }),
-        new CopyWebpackPlugin([
-            {from: 'public/manifest.json'},
-            {from: 'public/favicon.ico'},
-            {from: 'public/icons', to: 'static/icons'}
-        ])
-    ]
-};
+if (process.env.HMR === 'true') {
+  config.plugins = (config.plugins || []).concat([
+    new ExtensionReloader({
+      manifest: __dirname + '/src/manifest.json',
+    }),
+  ]);
+}
+
+function transformHtml(content) {
+  return ejs.render(content.toString(), {
+    ...process.env,
+  });
+}
+
+module.exports = config;
