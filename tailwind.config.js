ad3a2e4a57d5a56fc702f58d33f83fe9c0903388
Ryan Freeman <info@ryanfreeman.io>
2020-01-25 00:21:12 +0000

    Added config file for tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    theme: {
+        screens: {
+            'sm': '360px'
+        },
+        extend: {
+            fontFamily: {
+              sans: ['Nunito']
+            },
+            boxShadow: {
+              b: ' 0 6px 2px -2px rgba(0, 0, 0, .04)'
+            },
+            colors: {
+                safetyorange: '#ff9900',
+                mantis: '#72ca4f',
+                mineshaft: '#333333',
+                dovegrey: '#727272',
+                silver: '#bfbfbf',
+                alabaster: '#fafafa',
+                mercury: '#e6e6e4',
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+}
